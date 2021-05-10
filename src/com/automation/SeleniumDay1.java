36d340b8abf6e01f956f90f90c84fe122442f59c
Jyoti Sakhare <Jyoti Sakhare@DESKTOP-T6M756F>
2021-05-10 18:22:43 +0530

    Selenium Day1 Code by using By.id and By.name Locator is committed

---
+++ src/com/automation/SeleniumDay1.java
@@ -0,0 +1,48 @@
+package com.automation;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SeleniumDay1 {
+	WebDriver driver= null;
+	
+	public void intilizeWebEnviroment(){
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/externalResources/chromedriver.exe");
+	    driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+	    driver.manage().window().maximize();
+	}
+	
+	public void setUsername(){
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		
+		driver.findElement(By.id("email")).sendKeys("Jyoti");
+	}
+	
+	public void setPassword(){
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.findElement(By.name("pass")).sendKeys("Admin@123");
+	}
+	
+	public void tearDown(){
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		
+		driver.close();
+	}
+	
+	
+	public static void main(String[] args) {
+		SeleniumDay1 objSeleniumDay1=new SeleniumDay1();
+		objSeleniumDay1.intilizeWebEnviroment();
+		objSeleniumDay1.setUsername();
+		objSeleniumDay1.setPassword();
+		objSeleniumDay1.tearDown();
+		
+		
+	}
+
+}
